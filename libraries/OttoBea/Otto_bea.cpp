8d00c66546767499af3cc9835ffcd6abd5f99cca
kyberpunk <xvh@seznam.cz>
2017-08-02 19:58:38 +0200

    czech interface implementation

--- libraries/OttoBea/Otto_bea.cpp
+++ libraries/OttoBea/Otto_bea.cpp
@@ -3,16 +3,52 @@
 
 #define DURATION 1000
 
-Otto otto;
+Otto Otto;
 
-void Otto::probud_se() {
+void Robot::probud_se() {
 	Otto.init(PIN_YL, PIN_YR, PIN_RL, PIN_RR, true, -1, 10, 8, 9);
 }
 
-void Otto::srovnej_se() {
+void Robot::srovnej_se() {
 	Otto.home();
 }
 
-void Otto::jdi(int pocet_kroku, int smer) {
+void Robot::jdi(int pocet_kroku, int smer) {
 	Otto.walk(pocet_kroku, DURATION, smer);
+}
+
+void Robot::zatoc(int pocet_kroku, int strana) {
+	Otto.turn(pocet_kroku, 2000, strana);
+}
+
+void Robot::nahni_se(int strana) {
+	Otto.bend(1, 1400, strana);
+};
+
+void Robot::skoc() {
+	Otto.jump(1, 2000);
+}
+
+void Robot::zatres_nohou(int strana) {
+	Otto.shakeLeg(1, 2000, strana);
+}
+
+void Robot::moonwalk(int strana) {
+	Otto.moonwalker(1, 900, 20, strana);
+}
+
+float Robot::ziskej_vzdalenost() {
+	Otto.getDistance();
+}
+
+double Robot::stav_baterie() {
+	Otto.getBatteryLevel();
+}
+
+void Robot::udelej_gesto(int gesto) {
+	Otto.playGesture(gesto);
+}
+
+void Robot::zpivej(int zvuk) {
+	Otto.sing(zvuk);
 }
--- libraries/OttoBea/Otto_bea.h
+++ libraries/OttoBea/Otto_bea.h
@@ -15,24 +15,20 @@
 #define PRAVA 1
 #define LEVA-1
 
-// inicializace
-void probud_se();
-
-// pohyby
-void srovnej_se();
-void jdi(int pocet_kroku, int smer);
-void zatoc(int pocet_kroku, int strana);
-void nahni_se(int strana);
-void skoc();
-void zatres_nohou(int strana);
-
-// senzory
-float ziskej_vzdalenost();
-float stav_baterie();
+// zvuky
+#define Z_PRIPOJENY        0
+#define Z_ODPOJENY         1
+#define Z_PREKVAPENY       6
+#define Z_PRITULNY         9
+#define Z_OHOOH            7
+#define Z_SPANEK           10
+#define Z_STASTNY          11
+#define Z_VELMI_STASTNY    12
+#define Z_SMUTNY           14
+#define Z_ZMATENY          15
+#define Z_PRD              16
 
 // gesta
-void udelej_gesto(int gesto)
-
 #define G_STASTNY          0
 #define G_VELMI_STASTNY    1
 #define G_SMUTNY           2
@@ -44,18 +40,30 @@ void udelej_gesto(int gesto)
 #define G_SPANEK           3
 #define G_PODRAZDENY       8
 
-// zvuky
-void zpivej(int zvuk);
+class Robot
+{
+public:
+	// inicializace
+	void probud_se();
 
-#define Z_PRIPOJENY        0
-#define Z_ODPOJENY         1
-#define Z_PREKVAPENY       6
-#define Z_PRITULNY         9
-#define Z_OHOOH            7
-#define Z_SPANEK           10
-#define Z_STASTNY          11
-#define Z_VELMI_STASTNY    12
-#define Z_SMUTNY           14
-#define Z_ZMATENY          15
+	// pohyby
+	void srovnej_se();
+	void jdi(int pocet_kroku, int smer);
+	void zatoc(int pocet_kroku, int strana);
+	void nahni_se(int strana);
+	void skoc();
+	void zatres_nohou(int strana);
+	void moonwalk(int strana);
+
+	// senzory
+	float ziskej_vzdalenost();
+	double stav_baterie();
+
+	// gesta
+	void udelej_gesto(int gesto);
+
+	// zvuky
+	void zpivej(int zvuk);
+};
 
 #endif // OTTOBEA_H
