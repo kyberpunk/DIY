0de7fbc42cdff183efddeec0e3e7bbc06140f798
kyberpunk <xvh@seznam.cz>
2017-08-16 16:38:34 +0200

    prekazka sample

---
+++ OTTO_bea_prekazka/OTTO_bea_prekazka.ino
@@ -0,0 +1,24 @@
+#include <Otto_bea.h>
+
+Robot robot;
+
+void setup(){
+  // funkce setup se provede pouze jednou po spusteni programu
+  // spusteni robota
+  robot.probud_se();
+  // nastaveni do zakladni polohy
+  robot.srovnej_se();
+  // zvuk
+  robot.zpivej(Z_PRIPOJENY);
+}
+
+void loop() {  
+  // funkce loop se opakuje do nekonecna
+  // zde piste vase prikazy:
+
+  // zjisti vzdalenost prekazky a pokud je mensi nez 10 cm, tak udela gesto
+  if (robot.ziskej_vzdalenost() < 10.0)
+  {
+    robot.udelej_gesto(G_SMUTNY);
+  }
+}
